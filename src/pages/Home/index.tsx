63768c55b7ed96967b7574def8a7ae6a4c2aeb6c
LeO <devgru0114@gmail.com>
2022-12-21 04:47:39 -0500

    build layout

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+
+import Container from "../../components/container/Container";
+import CoinTable from "./components/CoinTable";
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <CoinTable />
+    </Container>
+  )
+}
+
+export default Dashboard
